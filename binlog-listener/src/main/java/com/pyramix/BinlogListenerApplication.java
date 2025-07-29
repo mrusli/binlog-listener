64d325143d3d452def2c41e16d40376a1d90dae6
mrusli <rusli1621@gmail.com>
2025-07-29 17:40:51 +0700

    init commit

---
+++ binlog-listener/src/main/java/com/pyramix/BinlogListenerApplication.java
@@ -0,0 +1,13 @@
+package com.pyramix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinlogListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinlogListenerApplication.class, args);
+	}
+
+}
---
+++ binlog-listener/src/main/java/com/pyramix/MyBinLogListener.java
@@ -0,0 +1,65 @@
+package com.pyramix;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.Event;
+import com.github.shyiko.mysql.binlog.event.EventData;
+import com.github.shyiko.mysql.binlog.event.EventType;
+import com.github.shyiko.mysql.binlog.event.TableMapEventData;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
+import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
+
+public class MyBinLogListener {
+
+	private static final Logger log = LoggerFactory.getLogger(MyBinLogListener.class);
+	
+	public static void main(String[] args) throws IllegalStateException, IOException {
+		log.info("HelloWorld!!!!");
+		BinaryLogClient client = new BinaryLogClient(
+					"localhost",
+					3304,
+					"e021_swi_core",
+					"root",
+					"password"
+				);
+		client.setServerId(2);
+		client.registerEventListener(new BinaryLogClient.EventListener() {
+			
+			@Override
+			public void onEvent(Event event) {
+				log.info("Received event type: {}", event.getHeader().getEventType());
+				
+				EventType eventType = event.getHeader().getEventType();
+				if (eventType == EventType.TABLE_MAP) {
+					TableMapEventData tableMapEventData = event.getData();
+					// long tableId = tableMapEventData.getTableId();
+					String databaseName = tableMapEventData.getDatabase();
+					String tableName = tableMapEventData.getTable();
+					log.info("database={}, table={}",databaseName,tableName);
+				}
+				if (eventType == EventType.EXT_UPDATE_ROWS) {
+					UpdateRowsEventData updateRowsEventData = event.getData();
+					log.info("update_row_id={}", (Object[])updateRowsEventData.getRows().getFirst().getValue());
+				}
+				if (eventType == EventType.EXT_WRITE_ROWS) {
+					WriteRowsEventData writeRowsEventData = event.getData();
+					log.info("new_row_id={}", (Object[])writeRowsEventData.getRows().getFirst());
+				}
+				
+				
+			}
+		});
+		client.connect();
+		log.info(String.format("%s", client.isConnected()));		
+	}
+
+}
